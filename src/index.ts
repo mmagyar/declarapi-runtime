bb80ff5b5e2ba2758b2356f919b29cc833948949
Magyar Máté <dev@mmagyar.com>
2020-08-18 06:04:26 +0200

    remove data connector export from index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 export * from './globalTypes'
 export { addValidationToContract } from './contractValidation.js'
 export { registerRestMethods } from './registerRestMethods.js'
-
-export * as elastic from './elastic.js'
-export * as kv from './kv.js'
